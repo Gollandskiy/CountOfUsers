6164dbc4ddafca2eb14c8a8bc6ea61f24b0a404a
nikita.prigolovkin <vitalii.prigolovkin@gmail.com>
2024-02-18 19:03:12 +0300

    Добавьте файлы проекта.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace CountOfUsers.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ISession _session;
+        private static Dictionary<string, bool> _activeSessions = new Dictionary<string, bool>();
+
+        public UsersController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _session = _httpContextAccessor.HttpContext.Session;
+        }
+
+        public IActionResult User()
+        {
+            int onlineUsersCount = _activeSessions.Count;
+            ViewBag.OnlineUsersCount = onlineUsersCount;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult KeepSessionAlive()
+        {
+            string sessionId = _session.Id;
+            if (!_activeSessions.ContainsKey(sessionId))
+            {
+                _activeSessions[sessionId] = true;
+            }
+            Console.WriteLine("KeepSessionAlive here");
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public IActionResult CloseSession()
+        {
+            string sessionId = _session.Id;
+            _activeSessions.Remove(sessionId);
+            Console.WriteLine("CloseSession here");
+            return new EmptyResult();
+        }
+    }
+}
